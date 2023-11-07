16969c6f21c5d19bdf7e77d9213125ef765c7b20
romain.fontugne <romain.fontugne@gmail.com>
2023-11-07 17:02:16 +0900

    Add kafka consumer to get data from a kafka topic

--- ip2asn.py
+++ ip2asn.py
@@ -1,34 +1,72 @@
-import sys
+import bz2
+from confluent_kafka import Consumer, KafkaError
+import json
+import msgpack
+import os
 import pickle
 import radix
-import json
-import bz2
+import sys
+
 
 class ip2asn:
-    def __init__(self, db="db/latest.pickle", ixp=None):
+    def __init__(self, db="db/latest.pickle", ixp=None, kafka_topic='', kafka_host='localhost:9092'):
 
         self.db = db
         self.asname = {}
-        self.rtree = pickle.load(bz2.BZ2File(db, "rb"))
+        self.rtree = radix.Radix()
 
-        if ixp is not None:
-            with open(ixp) as fi:
-                for line in fi:
-                    if not line.startswith("#"):
-                        ix = json.loads(line)
-                        for pfx in ix["prefixes"]["ipv4"]:
-                            node = self.rtree.add(pfx)
-                            # negative values for IXPs
-                            asn = ix["ix_id"]*-1
-                            node.data["as"] = asn
-                            self.asname[asn] = ix["name"]
+        if kafka_topic:
+            # Get data from Kafka topic
+            c = Consumer({
+                'bootstrap.servers': kafka_host,
+                'auto.offset.reset': 'earliest',
+                'enable.auto.commit': False,
+                'group.id': 'ip2asn_consumer',
+            })
+            c.subscribe([kafka_topic])
 
-                        for pfx in ix["prefixes"]["ipv6"]:
-                            node = self.rtree.add(pfx)
-                            # negative values for IXPs
-                            asn = ix["ix_id"]*-1
-                            node.data["as"] = asn
-                            self.asname[asn] = ix["name"]
+            while True:
+                msg = c.poll(timeout=10.0)
+
+                if msg is None:
+                    break
+
+                if msg.error():
+                    print("Consumer error: {}".format(msg.error()))
+                    continue
+
+                msg = {
+                       'prefix': msg.key().decode('utf-8'),
+                       'data': msgpack.unpackb(msg.value(), raw=False)
+                       }
+
+                rnode = self.rtree.add(msg['prefix'])
+                for k, v in msg['data'].items():
+                    rnode.data[k] = v
+
+            c.close()
+        else:
+            # get data from local files
+            self.rtree = pickle.load(bz2.BZ2File(db, "rb"))
+
+            if ixp is not None:
+                with open(ixp) as fi:
+                    for line in fi:
+                        if not line.startswith("#"):
+                            ix = json.loads(line)
+                            for pfx in ix["prefixes"]["ipv4"]:
+                                node = self.rtree.add(pfx)
+                                # negative values for IXPs
+                                asn = ix["ix_id"]*-1
+                                node.data["as"] = asn
+                                self.asname[asn] = ix["name"]
+
+                            for pfx in ix["prefixes"]["ipv6"]:
+                                node = self.rtree.add(pfx)
+                                # negative values for IXPs
+                                asn = ix["ix_id"]*-1
+                                node.data["as"] = asn
+                                self.asname[asn] = ix["name"]
 
 
     def ip2asn(self, ip):
@@ -86,7 +124,7 @@ class ip2asn:
             return ""
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv)<3:
         print("usage: %s yyyymm ip")
         sys.exit()
